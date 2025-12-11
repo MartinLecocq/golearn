58de99ce321dfffa331fdd2344e20a3c7b42256c
MartinLecocq <Martinl@hotmail.be>
2025-12-11 14:18:25 +0100

    Added some functions from the point 4.2 in the temp_umrechnung.go file.

--- temp_umrechnung.go
+++ temp_umrechnung.go
@@ -2,10 +2,19 @@ package main
 
 import "fmt"
 
+// Zusätzliche Funktion aus Punkt 4.2
+func fNachC(tempF float64) float64 {
+	return (tempF - 32) / 1.8 // Temperatur in Grad Celsius
+}
+
 func main() {
 	// Deklaration einer Variablen vom Typ float64
 	// und implizite Initialisierung mit Null-Wert
 	var tempC float64
+	// Zusätzliche Variablen aus Punkt 4.2
+	var tempF0 float64 = 0
+	var tempF1 float64 = 32
+	var tempF2 float64 = 96
 
 	// Deklaration einer Konstanten vom Typ float64
 	const nullpunkt float64 = -273.15
@@ -32,4 +41,10 @@ func main() {
 	fmt.Println(tempText+"°C:", tempC)
 	fmt.Println(tempText+"°F:", tempF)
 	fmt.Println(tempText+" K:", tempK)
+
+	// Beispiel aus Punkt 4.2
+	fmt.Println("\nBeispiel au Punkt 4.2:")
+	fmt.Println(tempF0, "°F entsprechen", fNachC(tempF0), "°C.")
+	fmt.Println(tempF1, "°F entsprechen", fNachC(tempF1), "°C.")
+	fmt.Println(tempF2, "°F entsprechen", fNachC(tempF2), "°C.")
 }

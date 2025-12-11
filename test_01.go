da6ef5eb0c2c15294fc73eb6713976aa86d318f5
MartinLecocq <Martinl@hotmail.be>
2025-12-11 16:28:39 +0100

    Tests on the Integer values in different counting systems in test_01.go.

--- test_01.go
+++ test_01.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
+	var aninteger int = 165_256
+	var anotherinteger int = 78
 	fmt.Println("Hi")
+	fmt.Println(aninteger)
+	fmt.Printf("%v:  %#[1]b  %#[1]o  %#[1]x\n", anotherinteger)
 }

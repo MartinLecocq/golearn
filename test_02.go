a54f57523336ab2c615d3d87ee20053d89127ac7
MartinLecocq <Martinl@hotmail.be>
2026-01-14 18:05:40 +0100

    Tests on understanding Zweierkomplement in Go.

--- test_02.go
+++ test_02.go
@@ -4,7 +4,8 @@ import "fmt"
 
 func main() {
 	var c int8 = -13
-	var cLeft int8 = c << 1
-	var cRight int8 = c >> 1
+	// var cLeft int8 = c << 1
+	// var cRight int8 = c >> 1
 	fmt.Printf("c:2-Kompl: %08b Dez: %3v\n", uint8(c), c)
+	fmt.Println(uint8(c))
 }

374570d4967260d20f566163e3c0ef5811d4678a
MartinLecocq <Martinl@hotmail.be>
2026-01-14 15:44:34 +0100

    More tests on the Integer values in different counting systems in test_01.go.

--- test_01.go
+++ test_01.go
@@ -7,5 +7,14 @@ func main() {
 	var anotherinteger int = 78
 	fmt.Println("Hi")
 	fmt.Println(aninteger)
-	fmt.Printf("%v:  %#[1]b  %#[1]o  %#[1]x\n", anotherinteger)
+	fmt.Printf("%v:  %#[1]b  %#[1]o  %#[1]x\n\n", anotherinteger)
+
+	var a int = 78
+	var b int = 0b1010_0011 // Dezimalzahl 163
+	// Ausgabe ohne Präfix
+	fmt.Printf("%v: %[1]b %[1]o %[1]x\n", a)
+	// Ausgabe mit Präfix
+	fmt.Printf("%d: %#[1]b %#[1]o %#[1]x\n", a)
+	// alternative Ausgabe
+	fmt.Printf("%v: %[1]b %[1]O %[1]X\n", b)
 }

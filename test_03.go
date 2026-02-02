5cc066e1fb797641d7b1e753f0e095113df1a307
MartinLecocq <Martinl@hotmail.be>
2026-02-02 16:19:44 +0100

    Testing the boolean type and the string type and some comparators.

--- test_03.go
+++ test_03.go
@@ -9,4 +9,13 @@ func main() {
 	fmt.Println(ergebnis)
 	// Die Mantisse ist bei float32 zwar viel großer als 8 bit (hier 23 bits). Deshalb ist die Rundungsfehler viel
 	// kleiner (0,000093%).
+
+	// Test auf Boolesche Velgeichsoperatoren für Strings
+	var bool_ergebnis = "bc" > "ad"
+	fmt.Println(bool_ergebnis)
+	// Test auf String Datentyp: Unicode Zeichen
+	var univar1 string = "Unendlich ist \u221e und Smiley ist \U0001f600"
+	var univar2 string = "Griechisch: \u0370\u0371\u037C"
+	fmt.Println(univar1)
+	fmt.Println(univar2)
 }

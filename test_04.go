8046aee5a499b54ec07577718b4ccb599a8ae6a3
MartinLecocq <Martinl@hotmail.be>
2026-03-25 15:38:53 +0100

    More tests on manipulating Strings with selecting specific characters via index and low:high indexes.

--- test_04.go
+++ test_04.go
@@ -7,6 +7,7 @@ import (
 
 // Variable declaration spot
 var mystring string = "Tür"
+var mybyte int = 0
 
 func main() {
 	fmt.Println(`Hello world!`)
@@ -14,7 +15,10 @@ func main() {
 	// Testing the counting of characters in a word with 2 different functions
 	fmt.Printf("Der String %q besteht aus %d Zeichen. Hoppla! Ich meine tatsächlich %d Zeichen.\n",
 		mystring, len(mystring), utf8.RuneCountInString(mystring))
-	// I now want to print my string in the hexadecimal form
+	// I now want to print my string in the hexadecimal form and showing an exact byte.
 	fmt.Printf("Der String %[1]q wird auf Hexadezimal so geschrieben: %[1]X\n", mystring)
-
+	fmt.Printf("Der Byte %v meines String sieht so auf Hexadezimal aus: %X\n", mybyte, mystring[mybyte])
+	// With the following function, I can print the letter as s String via a low:high index.
+	// But I still need to know how many bytes my character needs.
+	fmt.Printf("Die Buchstabe ist: %v", mystring[mybyte:mybyte+1])
 }

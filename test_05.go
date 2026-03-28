4975d6e7eec22bdaf68c083749edb309e83d6c37
MartinLecocq <Martinl@hotmail.be>
2026-03-28 12:55:41 +0100

    Tests in test_05.go file of data type conversion.

--- test_05.go
+++ test_05.go
@@ -1,11 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 var amountApples int = 5
 
 func main() {
-
+	fmt.Printf("The result is: %[1]v - The returned value is of type %[1]T.\n", float64(amountApples)/2)
+	fmt.Printf("The result is: %[1]v - The returned value is of type %[1]T.\n", amountApples/2)
+	fmt.Printf("The result is: %[1]v - The returned value is of type %[1]T.\n", float64(amountApples/2))
 }
